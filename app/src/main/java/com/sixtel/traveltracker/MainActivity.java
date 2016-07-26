5088d18ade3f1a8896cea971711ca20360ef800d
Branden Sandahl <branden157@gmail.com>
2016-07-26 10:55:37 -0400

    working on map

--- app/src/main/java/com/sixtel/traveltracker/MainActivity.java
+++ app/src/main/java/com/sixtel/traveltracker/MainActivity.java
@@ -1,13 +1,131 @@
 package com.sixtel.traveltracker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMapClickListener {
+
+    private static final int LOCATION_REQUEST_CODE = 200;
+
+    GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+        }
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    private void addGoogleAPIClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == LOCATION_REQUEST_CODE) {
+//            if (permissions.length == 1 &&
+//                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                mMap.setMyLocationEnabled(true);
+//            } else {
+//                // Permission was denied. Display an error message.
+//            }
+//        }
+//    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
+        mMap.setInfoWindowAdapter(new MarkerAdapter(getLayoutInflater()));
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> matches = null;
+
+        try {
+            matches = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Address bestMatch = (matches.isEmpty()) ? null : matches.get(0);
+        int maxLines = bestMatch.getMaxAddressLineIndex();
+
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(bestMatch.getAddressLine(maxLines - 1))
+                .snippet(bestMatch.getAddressLine(maxLines)));
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //commented out b/c doesnt work well with emulator,  but this is how to get a location on startup
+//        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        double latitiude = location.getLatitude();
+        double longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
---
+++ app/src/main/java/com/sixtel/traveltracker/MarkerAdapter.java
@@ -0,0 +1,43 @@
+package com.sixtel.traveltracker;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by branden on 7/26/16.
+ */
+public class MarkerAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private LayoutInflater mLayoutInflater;
+    private View mView;
+
+    MarkerAdapter(LayoutInflater layoutInflater) {
+        mLayoutInflater = layoutInflater;
+    }
+
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+
+        if (mView == null) {
+            mView = mLayoutInflater.inflate(R.layout.marker, null);
+        }
+
+        TextView titleView = (TextView) mView.findViewById(R.id.title);
+        titleView.setText(marker.getTitle());
+
+        TextView snippetView = (TextView) mView.findViewById(R.id.snippet);
+        snippetView.setText(marker.getSnippet());
+
+        return mView;
+    }
+}
